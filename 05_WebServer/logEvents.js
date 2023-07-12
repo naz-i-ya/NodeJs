70624987dfaebc4953fe6db3d91ac2fa134a9fc5
naz-i-ya <naziyanaz1208@gmail.com>
2023-07-13 02:36:59 +0530

    Web Server

---
+++ 05_WebServer/logEvents.js
@@ -0,0 +1,27 @@
+//Dependencies
+const { format } = require('date-fns');
+const { v4: uuid } = require('uuid');
+
+//common core modules
+const fs = require('fs');
+const fsPromises = require('fs').promises;
+const path = require('path');
+
+const logEvents = async (message, logName) => {
+    const dateTime = `${format(new Date(), 'yyyyMMdd\tHH:mm:ss')}`;
+    const logItem = `${dateTime}\t${uuid()}\t${message}\n\n`;
+    console.log(logItem);
+    try{
+        if(!fs.existsSync(path.join(__dirname, 'logs'))){
+            await fsPromises.mkdir(path.join(__dirname, 'logs'))
+        }
+        //testing
+        await fsPromises.appendFile(path.join(__dirname, 'logs', logName), logItem);
+    }
+    catch(err){
+        console.log(err)
+    }
+}
+
+module.exports = logEvents;
+
